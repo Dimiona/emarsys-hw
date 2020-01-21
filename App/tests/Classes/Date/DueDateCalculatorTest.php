0459258faf17bb45e2fe26768bf1a4b26fb5a44a
Tibor Pinter <dimiona@gmail.com>
2020-01-21 22:39:44 +0100

    Adds new unittests to DueDateCalculatorTest class.

--- App/tests/Classes/Date/DueDateCalculatorTest.php
+++ App/tests/Classes/Date/DueDateCalculatorTest.php
@@ -30,4 +30,28 @@ class DueDateCalculatorTest extends TestCase {
     $this->assertInstanceOf(\DateTimeInterface::class, $resolveDate);
   }
 
+  public function test2DaysTurnaroundTime() {
+    $submitDate = new \DateTime('2020-01-21 14:12:00');
+    $turnaroundTime = 16;
+    $resolveDate = $this->calculator->CalculateDueDate($submitDate, $turnaroundTime);
+
+    $this->assertEquals('2020-01-23 14:12:00', $resolveDate->format('Y-m-d H:i:s'));
+  }
+
+  public function testHasOneWeekendInIt() {
+    $submitDate = new \DateTime('2020-01-21 16:12:00');
+    $turnaroundTime = 25;
+    $resolveDate = $this->calculator->CalculateDueDate($submitDate, $turnaroundTime);
+
+    $this->assertEquals('2020-01-27 09:12:00', $resolveDate->format('Y-m-d H:i:s'));
+  }
+
+  public function testHasTwoWeekendsInIt() {
+    $submitDate = new \DateTime('2020-01-21 16:12:00');
+    $turnaroundTime = 65;
+    $resolveDate = $this->calculator->CalculateDueDate($submitDate, $turnaroundTime);
+
+    $this->assertEquals('2020-02-03 09:12:00', $resolveDate->format('Y-m-d H:i:s'));
+  }
+
 }
