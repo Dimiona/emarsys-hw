b4d6c0d69ff7db2e3830773e72e2dbc2ed79fb43
Tibor Pinter <dimiona@gmail.com>
2020-01-21 14:00:29 +0100

    CalculateDueDate method's implementation started. Checks for parameters, first. And then start to calculate resolve date/time.

--- Classes/Date/DueDateCalculator.php
+++ Classes/Date/DueDateCalculator.php
@@ -2,6 +2,9 @@
 
 namespace Classes\Date;
 
+use Classes\Exception\TurnaroundTimeException;
+use Classes\Exception\WorkingHoursException;
+
 /**
  * Class DueDateCalculator.
  *
@@ -13,7 +16,27 @@ class DueDateCalculator extends DueDateCalculatorBase implements DueDateCalculat
    * {@inheritdoc}
    */
   public function CalculateDueDate(\DateTime $submitDate, int $turnaroundTime): \DateTime {
-    return new \DateTime('2019-12-01 09:15:00');
+    if (!$this->isWorkingHours($submitDate)) {
+      throw new WorkingHoursException('The submission date is out of working hours.');
+    }
+
+    if ($turnaroundTime < 1) {
+      throw new TurnaroundTimeException('Invalid turnaround time. It must be greater or equal than 1.');
+    }
+
+    $workingHours = $this->getWorkingHours();
+    $resolveDate = clone $submitDate;
+    while ($turnaroundTime > 0) {
+      $hours = (int) $workingHours;
+      $minutes = ($workingHours - $hours) * 60;
+
+      $addition = new \DateInterval('PT' . $hours . 'H' . $minutes . 'M');
+      $resolveDate->add($addition);
+
+      $turnaroundTime -= $workingHours;
+    }
+
+    return $resolveDate;
   }
 
 }
