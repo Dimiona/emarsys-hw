2de90eb3f640a3f598858b530d23e5eb6ec5819e
Tibor Pinter <dimiona@gmail.com>
2020-01-21 21:44:40 +0100

    Adds alternate autoload functionality with composer.

--- index.php
+++ index.php
@@ -5,6 +5,9 @@ use App\Classes\Factory\DueDateCalculatorFactory;
 // Basic autoload.
 spl_autoload_register();
 
+// Autoload with composer.
+// require_once 'vendor/autoload.php';
+
 // Example of use.
 $submitDate = new \DateTime('2020-01-21 14:12:00');
 $turnaroundTime = 16;
